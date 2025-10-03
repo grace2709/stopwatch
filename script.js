245ac077788bd1a29b6ecb096ce45e7031d59826
Chiamaka  Anyanwu <93395990+grace2709@users.noreply.github.com>
2025-10-03 08:33:45 +0100

    added comments

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ let [hours, minutes, seconds] = [0, 0, 0];
 let isRunning = false;
 
 
+//code to set the interval
+
 document.getElementById("start").addEventListener("click", () => {
     if(!isRunning){
         isRunning = true;
         timer = setInterval(runTimer, 1000);
     }
 });
-
+//this code sets the "stop" timer
 document.getElementById("stop").addEventListener("click", () => {
     clearInterval(timer);
     isRunning = false;
@@ -30,7 +32,7 @@ function runTimer() {
     seconds = 0;
     minutes++;
   }
-  if (minutes === 60) {
+  if (minutes === 60) { //if miinutes equals 60, then minutes = 0; hour increases by 1
     minutes = 0;
     hours++;
   }
@@ -39,5 +41,6 @@ function runTimer() {
   let m = minutes < 10 ? "0" + minutes : minutes;
   let s = seconds < 10 ? "0" + seconds : seconds;
 
-  display.innerHTML = `${h}:${m}:${s}`;
+  display.innerHTML = `${h}:${m}:${s}`;   //this works with the html filw
+
 }

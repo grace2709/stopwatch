999ddc3ed627cc5d17589ff3d217e914addb49cc
Chiamaka  Anyanwu <93395990+grace2709@users.noreply.github.com>
2025-10-03 08:02:32 +0100

    added files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let timer;
+let [hours, minutes, seconds] = [0, 0, 0];
+let isRunning = false;
+
+
+document.getElementById("start").addEventListener("click", () => {
+    if(!isRunning){
+        isRunning = true;
+        timer = setInterval(runTimer, 1000);
+    }
+});
+
+document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(timer);
+    isRunning = false;
+});
+
+//reset
+
+document.getElementById("reset").addEventListener("click", () => {
+    clearInterval(timer);
+    [hours, minutes, seconds] = [0, 0, 0];
+    display.innerHTML = "00:00:00";
+    isRunning = false;
+    
+});
+function runTimer() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+  if (minutes === 60) {
+    minutes = 0;
+    hours++;
+  }
+
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  display.innerHTML = `${h}:${m}:${s}`;
+}
